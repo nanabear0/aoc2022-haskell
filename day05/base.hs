168b932c51582eafc964ecc04f0e751d2a2bc52e
Denizhan Erdem <derdem@microsoft.com>
2023-08-29 17:39:38 +0100

    day05

---
+++ day05/base.hs
@@ -0,0 +1,53 @@
+import Data.Char (isDigit, isSpace)
+import Data.List (elemIndex, transpose, unfoldr)
+import Data.Maybe (fromMaybe)
+import System.IO (readFile)
+
+replace2 :: [String] -> Int -> Int -> String -> String -> [String]
+replace2 [] _ _ _ _ = []
+replace2 (ns : state) 0 y nx ny = nx : replace2 state (-1) (y - 1) nx ny
+replace2 (ns : state) x 0 nx ny = ny : replace2 state (x - 1) (-1) nx ny
+replace2 (ns : state) x y nx ny = ns : replace2 state (x - 1) (y - 1) nx ny
+
+updateState :: Bool -> [String] -> Int -> Int -> Int -> [String]
+updateState insertInReverse state n s t = replace2 state (s - 1) (t - 1) newSource newTarget
+  where
+    sourceStack = state !! (s - 1)
+    targetStack = state !! (t - 1)
+    newSource = drop n sourceStack
+    newTarget = (if insertInReverse then reverse else id) (take n sourceStack) ++ targetStack
+
+runOp :: Bool -> ([String], [(Int, Int, Int)]) -> [String]
+runOp _ (x, []) = x
+runOp insertInReverse (x, (n, s, t) : rest) = runOp insertInReverse (updateState insertInReverse x n s t, rest)
+
+readOperations :: [String] -> [(Int, Int, Int)]
+readOperations x = toTuple . map read . filter (all isDigit) . words <$> x
+  where
+    toTuple [x, y, z] = (x, y, z)
+
+readState :: [String] -> [String]
+readState = map (filter (not . isSpace)) . transpose . map (map (!! 1) . chunks 4)
+  where
+    chunks n = takeWhile (not . null) . unfoldr (Just . splitAt n)
+
+readInput :: String -> ([String], [(Int, Int, Int)])
+readInput x = (readState $ init firstPart, readOperations $ tail secondPart)
+  where
+    l = lines x
+    separator = fromMaybe (length l) (elemIndex "" l)
+    (firstPart, secondPart) = splitAt separator l
+
+readTops :: [String] -> String
+readTops = map head
+
+part1 :: String -> String
+part1 = readTops . runOp True . readInput
+
+part2 :: String -> String
+part2 = readTops . runOp False . readInput
+
+main = do
+  contents <- readFile "input.txt"
+  putStrLn $ "part1: " ++ show (part1 contents)
+  putStrLn $ "part2: " ++ show (part2 contents)
